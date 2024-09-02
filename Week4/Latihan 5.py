be1b97adb4fdca5273c1c897d1b8f57e164c4066
Haikal-52 <muhammadhaikalhidayat@mail.ugm.ac.id>
2024-09-02 14:33:47 +0700

    Add files via upload

---
+++ Week4/Latihan 5.py
@@ -0,0 +1,34 @@
+nama = str(input("Masukkan nama = "))
+nilai = int(input("Masukan nilai = "))
+if (nilai>=70):
+    print("Lulus")
+else:
+    print("Tidak Lulus")
+
+nama = str(input("Masukkan nama = "))
+nilai = int(input("Masukan nilai = "))
+if (nilai>=70):
+    print("Lulus")
+else:
+    print("Tidak Lulus")
+
+nama = str(input("Masukkan nama = "))
+nilai = int(input("Masukan nilai = "))
+if (nilai>=70):
+    print("Lulus")
+else:
+    print("Tidak Lulus")
+
+nama = str(input("Masukkan nama = "))
+nilai = int(input("Masukan nilai = "))
+if (nilai>=70):
+    print("Lulus")
+else:
+    print("Tidak Lulus")
+
+nama = str(input("Masukkan nama = "))
+nilai = int(input("Masukan nilai = "))
+if (nilai>=70):
+    print("Lulus")
+else:
+    print("Tidak Lulus")
