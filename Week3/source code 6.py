b3e8ad3ead7f03fa85d3025494742bc28fcad917
Haikal-52 <muhammadhaikalhidayat@mail.ugm.ac.id>
2024-09-02 12:31:58 +0700

    Add files via upload

---
+++ Week3/source code 6.py
@@ -0,0 +1,4 @@
+a = "Dear Alice\nHow have you been? I am fine.\nThere is a container in the fridge\nthat is labeled 'Milk Experiment'.\nPlease do not drink it.\nSincerely,\nBob"
+hasil = a.split('\n')
+for baris in hasil:
+    print(baris)
