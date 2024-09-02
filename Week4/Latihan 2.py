94b0f2a909b81da26b3856988e5f1eabe1c1d85b
Haikal-52 <muhammadhaikalhidayat@mail.ugm.ac.id>
2024-09-02 14:01:30 +0700

    Add files via upload

---
+++ Week4/Latihan 2.py
@@ -0,0 +1,16 @@
+#Nama file: if_statement.py
+hari = 3
+if (hari==1):
+    print ("Hari Senin")
+elif (hari==2):
+    print ("Hari Selasa")
+elif (hari==3):
+    print ("Hari Rabu")
+elif (hari==4):
+    print ("Hari Kamis")
+elif (hari==5):
+    print ("Hari Jumat")
+elif (hari==6):
+    print("Hari Sabtu")
+else :
+    print ("Hari Minggu")             
---
+++ Week4/Latihan 5.py
@@ -0,0 +1,6 @@
+nama = input("Masukkan nama = ")
+nilai = int(input("Masukan nilai = "))
+if (nilai>=70) :
+    print("lulus")
+else : 
+    print("tidak lulus")
